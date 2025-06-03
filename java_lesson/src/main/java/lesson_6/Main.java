bd84a79c09a46f0e60bd2df763245d3c8e077616
ChibisIs <132361513+ChibisIs@users.noreply.github.com>
2025-06-03 18:22:50 +0400

    Lesson_6_ex_1

---
+++ java_lesson/src/main/java/lesson_6/Main.java
@@ -0,0 +1,19 @@
+package lesson_6;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
+
+
+public class Main {
+     public static void main(String[] args) {
+        Set<Student> students = new HashSet<>();
+        students.add(new Student("John", 1, 1, new ArrayList<>(List.of(4, 6, 3))));
+        students.add(new Student("Alex", 1, 1, new ArrayList<>(List.of(2, 1, 2))));
+        students.add(new Student("Maria", 1, 2, new ArrayList<>(List.of(5, 1, 5))));
+        Student.printStudents(students, 1); 
+        Student.expelStudent(students);
+        Student.transferStudent(students);
+        Student.printStudents(students, 2);
+    }
+}
