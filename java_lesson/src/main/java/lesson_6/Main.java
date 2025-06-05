18fdcf302ee8a7881342bf08fbe3a3bee6622227
ChibisIs <132361513+ChibisIs@users.noreply.github.com>
2025-06-05 22:25:23 +0400

    lesson_6_ex_2

--- java_lesson/src/main/java/lesson_6/Main.java
+++ java_lesson/src/main/java/lesson_6/Main.java
@@ -8,9 +8,9 @@ import java.util.HashSet;
 public class Main {
      public static void main(String[] args) {
         Set<Student> students = new HashSet<>();
-        students.add(new Student("John", 1, 1, new ArrayList<>(List.of(4, 6, 3))));
-        students.add(new Student("Alex", 1, 1, new ArrayList<>(List.of(2, 1, 2))));
-        students.add(new Student("Maria", 1, 2, new ArrayList<>(List.of(5, 1, 5))));
+        students.add(new Student("John", 2, 2, new ArrayList<>(List.of(4, 4, 3))));
+        students.add(new Student("Alex", 1, 1, new ArrayList<>(List.of(2, 3, 2))));
+        students.add(new Student("Maria", 1, 1, new ArrayList<>(List.of(5, 4, 5))));
         Student.printStudents(students, 1); 
         Student.expelStudent(students);
         Student.transferStudent(students);
