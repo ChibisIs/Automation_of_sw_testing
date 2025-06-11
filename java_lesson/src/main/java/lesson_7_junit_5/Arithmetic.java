64affcd888a4331a9c4850cddf22feb109beddcd
ChibisIs <132361513+ChibisIs@users.noreply.github.com>
2025-06-12 00:01:33 +0400

    Lesson_7_junit_5

--- java_lesson/src/main/java/lesson_7_junit_5/Arithmetic.java
+++ java_lesson/src/main/java/lesson_7_junit_5/Arithmetic.java
@@ -1,7 +1,7 @@
 package lesson_7_junit_5;
 
 public class Arithmetic {
-    public static long factorial(int n) {
+    public long factorial(int n) {
         if (n < 0) {
             throw new IllegalArgumentException("Отрицательные числа не допускаются");
         }
@@ -12,7 +12,7 @@ public class Arithmetic {
         return result;
     }
 
-    public static double triangleArea(double a, double b, double c) {
+    public double triangleArea(double a, double b, double c) {
         if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || b + c <= a) {
             throw new IllegalArgumentException("Такого треугольника не существует");
         }
@@ -20,24 +20,39 @@ public class Arithmetic {
         return Math.sqrt(p * (p - a) * (p - b) * (p - c));
     }
 
-    public static void arithmetic(int a, int b) {
-        System.out.println("Сумма: " + (a + b));
-        System.out.println("Разность: " + (a - b));
-        if (b != 0) {
-            System.out.println("Деление: " + ((double)a / b));
-        } else {
-            System.out.println("Деление: на ноль делить нельзя");
+    public class ArithmeticResult {
+        public int sum;
+        public int diff;
+        public int mult;
+        public Double div;
+
+        public ArithmeticResult(int sum, int diff, int mult, Double div) {
+            this.sum = sum;
+            this.diff = diff;
+            this.mult = mult;
+            this.div = div;
         }
-        System.out.println("Умножение: " + (a * b));
     }
 
-    public static void compare(int a, int b) {
+    public ArithmeticResult arithmeticOperations(int a, int b) {
+        ArithmeticResult result = new ArithmeticResult(0, 0, 0, null);
+        int sum = a + b;
+        int diff = a - b;
+        int mult = a * b;
+        Double div = (b != 0) ? ((double) a / b) : null;
+        return result = new ArithmeticResult(sum, diff, mult, div);
+    }
+
+    public int compare(int a, int b) {
         if (a > b) {
             System.out.println(a + " > " + b);
+            return 1;
         } else if (a < b) {
             System.out.println(a + " < " + b);
+            return -1;
         } else {
             System.out.println(a + " = " + b);
+            return 0;
         }
     }
 }
